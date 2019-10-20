882a28643980664eb9752a94f1161e0ec87cf2cf
Adrian Romański <muszynianka1337@gmail.com>
2019-10-20 10:24:36 +0200

    Refectoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/adrianromanski/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/adrianromanski/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package adrianromanski.sfgpetclinic.services;
 
 import adrianromanski.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
