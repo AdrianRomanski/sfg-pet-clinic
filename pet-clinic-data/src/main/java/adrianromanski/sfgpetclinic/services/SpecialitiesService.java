a394e511ceebf089bbb3a5060acf6d87afcf5f35
Adrian Romański <muszynianka1337@gmail.com>
2020-02-11 13:20:07 +0100

    Added Specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/adrianromanski/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package adrianromanski.sfgpetclinic.services;
+
+import adrianromanski.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
