67c403039516ad599c1430291c225e3fac83afa9
Adrian Romański <muszynianka1337@gmail.com>
2020-02-16 21:49:50 +0100

    Create Spring Data JPA service for Pets. Closes #43

--- pet-clinic-data/src/main/java/adrianromanski/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/adrianromanski/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -41,7 +41,7 @@ public class OwnerSDJpaService implements OwnerService {
 
     @Override
     public Owner findById(Long aLong) {
-        return ownerRepository.findById(aLong).orElse(null);
+        return ownerRepository.findById(aLong).orElseThrow(null);
     }
 
     @Override
