048a443c370660b361c8658f6935669b19e9e675
Adrian Romański <muszynianka1337@gmail.com>
2020-02-18 14:36:36 +0100

    Added Project Lombok to project. Closes #51

--- pet-clinic-data/src/main/java/adrianromanski/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/adrianromanski/sfgpetclinic/model/Pet.java
@@ -1,13 +1,22 @@
 package adrianromanski.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "pets")
-public class Pet extends BaseEntity{
+public class Pet extends BaseEntity {
 
     @Column(name = "name")
     private String name;
@@ -25,36 +34,4 @@ public class Pet extends BaseEntity{
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
-
-    public Set<Visit> getVisits() { return visits; }
-
-    public void setVisits(Set<Visit> visits) { this.visits = visits; }
-
-    public String getName() { return name; }
-
-    public void setName(String name) { this.name = name; }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
 }
