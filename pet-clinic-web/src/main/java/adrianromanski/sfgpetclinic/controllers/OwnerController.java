89d3324b0efddbfd06cd31aa05ed5abd6ad4e497
Adrian Romański <muszynianka1337@gmail.com>
2020-03-06 11:54:51 +0100

    Added ability to add or update a Pet. Closes #56

--- pet-clinic-web/src/main/java/adrianromanski/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/adrianromanski/sfgpetclinic/controllers/OwnerController.java
@@ -24,7 +24,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @InitBinder
+    @InitBinder("owner")
     public void setAllowedFields(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
     }
--- pet-clinic-web/src/main/java/adrianromanski/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/adrianromanski/sfgpetclinic/controllers/PetController.java
@@ -1,31 +1,35 @@
 package adrianromanski.sfgpetclinic.controllers;
 
 import adrianromanski.sfgpetclinic.model.Owner;
+import adrianromanski.sfgpetclinic.model.Pet;
 import adrianromanski.sfgpetclinic.model.PetType;
 import adrianromanski.sfgpetclinic.services.OwnerService;
 import adrianromanski.sfgpetclinic.services.PetService;
 import adrianromanski.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Collection;
 
 @Controller
-@RequestMapping("owners/{ownerId}")
+@RequestMapping("/owners/{ownerId}")
 public class PetController {
 
-    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdateForm";
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
     private final PetService petService;
     private final OwnerService ownerService;
     private final PetTypeService petTypeService;
 
-    public PetController(PetService pets, OwnerService owners, PetTypeService petTypeService) {
-        this.petService = pets;
-        this.ownerService = owners;
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
         this.petTypeService = petTypeService;
     }
 
@@ -44,5 +48,50 @@ public class PetController {
         dataBinder.setDisallowedFields("id");
     }
 
+    @GetMapping("/pets/new")
+    public String initCreationForm(Owner owner, Model model) {
+        Pet pet = new Pet();
+        owner.getPets().add(pet);
+        pet.setOwner(owner);
+        model.addAttribute("pet", pet);
+        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/pets/new")
+    public String processCreationForm(Owner owner, @Valid Pet pet, BindingResult result, ModelMap model) {
+        if (StringUtils.hasLength(pet.getName()) && pet.isNew() && owner.getPet(pet.getName(), true) != null){
+            result.rejectValue("name", "duplicate", "already exists");
+        }
+        owner.getPets().add(pet);
+        if (result.hasErrors()) {
+            model.put("pet", pet);
+            return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+        } else {
+            pet.setOwner(owner);
+            petService.save(pet);
+
+            return "redirect:/owners/" + owner.getId();
+        }
+    }
+
+    @GetMapping("/pets/{petId}/edit")
+    public String initUpdateForm(@PathVariable Long petId, Model model) {
+        model.addAttribute("pet", petService.findById(petId));
+        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/pets/{petId}/edit")
+    public String processUpdateForm(@Valid Pet pet, BindingResult result, Owner owner, Model model) {
+        if (result.hasErrors()) {
+            pet.setOwner(owner);
+            model.addAttribute("pet", pet);
+            return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+        } else {
+            owner.getPets().add(pet);
+            pet.setOwner(owner);
+            petService.save(pet);
+            return "redirect:/owners/" + owner.getId();
+        }
+    }
 
 }
